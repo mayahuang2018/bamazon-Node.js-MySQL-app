0c4a447442031e256e58693911e9157c964d6bfc
Maya Chen <fangnihuang2016@gmail.com>
2019-05-15 16:17:06 -0400

    npm

---
+++ bamazonCustomer.js
@@ -0,0 +1,8 @@
+// Running this application will first display all of the items available for sale.
+//  Include the ids, names, and prices of products for sale.
+
+
+// The app should then prompt users with two messages.
+
+// The first should ask them the ID of the product they would like to buy.
+// The second message should ask how many units of the product they would like to buy.
