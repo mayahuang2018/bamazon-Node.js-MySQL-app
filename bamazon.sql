5191467f2546a6a9153ce11cc8edec1c237918a9
Maya Chen <fangnihuang2016@gmail.com>
2019-05-15 21:39:44 -0400

    start function

--- bamazon.sql
+++ bamazon.sql
@@ -15,10 +15,10 @@ CREATE TABLE products (
   item_id INT NOT NULL AUTO_INCREMENT,
 
   -- Makes a string column called "product_name (Name of product)" which cannot contain null --
-  product_name VARCHAR(10) NOT NULL,
+  product_name VARCHAR(100) NOT NULL,
  
   -- department_name
-  department_name VARCHAR(10) NOT NULL,
+  department_name VARCHAR(100) NOT NULL,
 
   --  price (cost to customer)
   price DECIMAL(10,2) NULL,
@@ -27,37 +27,39 @@ CREATE TABLE products (
   
   stock_quantity INT NULL
 
+  PRIMARY KEY (id)
+
 );
 
 
 -- Populate this database with around 10 different products. (i.e. Insert "mock" data rows into this database and table).
-INSERT INTO products (product_name，department_name，price，stock_quantity)
+INSERT INTO products (product_name,department_name,price,stock_quantity)
 VALUES ("The Design of Everyday Things","Book",10.43,6);
 
-INSERT INTO products (product_name，department_name，price，stock_quantity)
+INSERT INTO products (product_name,department_name,price,stock_quantity)
 VALUES ("Benefiber fiber supplement Power","Food",8.99,10);
 
-INSERT INTO products (product_name，department_name，price，stock_quantity)
+INSERT INTO products (product_name,department_name,price,stock_quantity)
 VALUES ("Bounty Quick-Size Paper Towels","Life",21.99,189);
 
-INSERT INTO products (product_name，department_name，price，stock_quantity)
+INSERT INTO products (product_name,department_name,price,stock_quantity)
 VALUES ("24 Bags instant Rice Noodle Chicken Flavour","Food",22.00,2);
 
-INSERT INTO products (product_name，department_name，price，stock_quantity)
+INSERT INTO products (product_name,department_name,price,stock_quantity)
 VALUES ("Gorilla Super Glue Gel","Life",5.97,29);
 
-INSERT INTO products (product_name，department_name，price，stock_quantity)
+INSERT INTO products (product_name,department_name,price,stock_quantity)
 VALUES ("Montblanc Rollerball Refills","Life",20.75,3);
 
-INSERT INTO products (product_name，department_name，price，stock_quantity)
+INSERT INTO products (product_name,department_name,price,stock_quantity)
 VALUES ("IKproductpro Makeup Mirror","Life",29.99,12);
 
-INSERT INTO products (product_name，department_name，price，stock_quantity)
+INSERT INTO products (product_name,department_name,price,stock_quantity)
 VALUES ("Everything & Everywhere: A Fact-Filled Adventure for Curious Globe-Trotters","Book",13.37,34);
 
-INSERT INTO products (product_name，department_name，price，stock_quantity)
+INSERT INTO products (product_name,department_name,price,stock_quantity)
 VALUES ("Baby Memory Book by Tiny Gifts","Book",22.95,6);
 
-INSERT INTO products (product_name，department_name，price，stock_quantity)
+INSERT INTO products (product_name,department_name,price,stock_quantity)
 VALUES ("Inomata Raw Rice Storage Container","Life",29.95,15);
 
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,8 +1,79 @@
+// connect node.js and mysql
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+var Table = require("cli-table");
+
+var connection = mysql.createConnection({
+  host     : "localhost",
+  port     : 3306,
+  user     : "root",
+  password : "docker",
+  database : "bamazon_db"
+});
+ 
+connection.connect(function(err){
+    if(err)
+    {
+        console.log(err);
+    }
+    console.log("COnnected as id: "+connection.threadId);
+    displayProducts();
+});
+ 
+/*connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+  if (error) throw error;
+  console.log('The solution is: ', results[0].solution);
+  displayProducts();
+});*/
+
+
 // Running this application will first display all of the items available for sale.
 //  Include the ids, names, and prices of products for sale.
+
+function displayProducts(){
+    console.log("===========================================");
+    console.log("Items available for sale");
+    console.log("===========================================");
+
+    connection.query("SELECT * FROM products", function(err,data){
+        var table = new Table({
+            head: ["ID","Item Name","Department Name","Price","Stock Quantity"]
+        });
+
+        for(var i=0;i<data.length;i++){
+            var arr = [];
+            for(var key in data[i]){
+                arr.push(data[i][key]);
+            }
+            table.push(arr);
+        }
+        console.log(table.toString());
+        palceOrder();
+    })
+}
 
 
 // The app should then prompt users with two messages.
 
 // The first should ask them the ID of the product they would like to buy.
 // The second message should ask how many units of the product they would like to buy.
+
+function palceOrder(){
+    inquirer.prompt([{
+        name: "id",
+        type: "input",
+        message: "Please enter product ID you want to buy: "
+    },{
+        name:"quantity",
+        type:"input",
+        message: "Please enter quantity:"
+    }]).then(function(answer){
+        console.log(answer);
+    })
+
+    connection.end();
+}
+
+
+
+
